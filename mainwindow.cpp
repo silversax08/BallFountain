3930dc3bc5213daacad9c567e3b50af027a8a1ef
Garrett Hawkins <garrett.hawkins@icloud.com>
2020-11-03 22:11:29 -0700

    cleaned mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindowform.h"
-#include "OSGWidget.h"
-#include "sphereupdatecallback.h"
-#include "UpdateBallPhysics.hpp"
-#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow{parent},
     mMainWindowUI{new Ui::MainWindowForm}
 {
     mMainWindowUI->setupUi(this);
-//    mMainwindowUI->pushButton;
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -11,25 +11,17 @@ class MainWindowForm;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
 public slots:
     void on_actionExit_triggered();
-
 private slots:
     void on_LaunchButton_clicked();
-
     void on_VelocitySlider_sliderMoved(int position);
-
     void on_AngleSlider_sliderMoved(int position);
-
     void on_VelocitySlider_valueChanged(int value);
-
     void on_AngleSlider_valueChanged(int value);
-
 private:
     Ui::MainWindowForm *mMainWindowUI;
     int velocity{7};
